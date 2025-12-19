31acd18dcc3c4e61197a061df71af59a64a8502e
brilliahib <brilliahib21@gmail.com>
2025-12-19 19:52:34 +0700

    feat(sub-modules): add sub modules pages for users

---
+++ src/components/molecules/card/sub-modules/CardListSubModules.tsx
@@ -0,0 +1,73 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { SubModules } from "@/types/sub-modules/sub-modules";
+import { Bookmark } from "lucide-react";
+import Link from "next/link";
+
+interface CardListSubModulesProps {
+  data: SubModules[];
+  isPending?: boolean;
+}
+
+export const CardListSubModulesSkeleton = () => {
+  return (
+    <>
+      <div className="my-6 grid grid-cols-1 gap-6">
+        <div className="flex gap-6">
+          <Skeleton className="h-36 w-36 rounded-xl md:flex hidden" />
+          <Skeleton className="h-[150px] w-full rounded-xl" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default function CardListSubModules({
+  data,
+  isPending,
+}: CardListSubModulesProps) {
+  if (isPending) {
+    return <CardListSubModulesSkeleton />;
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-6">
+      {data?.length === 0 ? (
+        <div className="text-center text-muted-foreground">
+          Sub Modul tidak ditemukan.
+        </div>
+      ) : (
+        data?.map((subModule) => (
+          <Link
+            href={`/dashboard/modules/${subModule.id}/contents`}
+            key={subModule.id}
+            className="group block"
+          >
+            <div className="flex flex-row gap-6">
+              <div className="relative hidden aspect-video h-36 w-36 items-center justify-center rounded-lg bg-primary group-hover:bg-primary/80 md:flex">
+                <Bookmark className="m-auto h-12 w-12 text-background" />
+              </div>
+              <Card className="w-full border-2 border-muted shadow-transparent group-hover:bg-muted">
+                <CardHeader className="flex md:flex-row md:items-center md:justify-between">
+                  <div className="space-y-1">
+                    <CardTitle className="text-md font-bold md:text-xl capitalize">
+                      {subModule.title}
+                    </CardTitle>
+                    <CardDescription className="text-sm text-muted-foreground line-clamp-2">
+                      {subModule.description}
+                    </CardDescription>
+                  </div>
+                </CardHeader>
+              </Card>
+            </div>
+          </Link>
+        ))
+      )}
+    </div>
+  );
+}
